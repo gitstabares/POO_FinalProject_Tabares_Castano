4a0b0068868b817ece2a6d71021b44dd5ba42372
jaco1127 <jacobocastano1127@gmail.com>
2025-11-21 16:02:16 -0500

    Implementation of Customer and Sale Classes

--- domain/Customer.java
+++ domain/Customer.java
@@ -1,5 +1,46 @@
 package domain;
+// Represents a customer in the system
 
+import java.util.ArrayList;
 public class Customer {
-    
+    private String name;
+    private int id;
+    private ArrayList<Videogame> gameLibrary;
+    private ArrayList<Sale> sales;
+
+    // Constructor
+
+    public Customer(String name, int id) {
+        this.name = name;
+        this.id = id;
+        this.gameLibrary = new ArrayList<>();
+    }
+
+    //Getters of the variables
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<Videogame> getLibrary() {
+        return gameLibrary;
+    }
+
+    public ArrayList<Sale> getSales() {
+        return sales;
+    }
+
+    //Method to add games purchased to the customer's library
+
+    public void addGame(ArrayList<Videogame> game) {
+        gameLibrary.addAll(game);
+    }
+
+    public void addSale(Sale sale) {
+        sales.add(sale);
+    }
 }
